018b87f9d556bb77895ce8396462517c8694030f
MILLOT Anthony <millotanth@groupe-mb.net>
2017-10-03 13:48:52 +0200

    Face params

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
+    $('.has-option').click(function () {
+        if($(this).data('for')){
+            var For = $(this).data('for');
 
+            $('#'+For).toggleClass('on');
+        }
+    });
 });
